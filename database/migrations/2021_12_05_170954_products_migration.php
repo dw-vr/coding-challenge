2007a8b4cdc0aff6f82e28e65da841fb55f4bb6a
Roger G Coscojuela <admin@rogergili.com>
2021-12-05 19:44:06 +0100

    Created migrations and removed example ones
    Created migrations for all models.

--- database/migrations/2021_12_05_170954_products_migration.php
+++ database/migrations/2021_12_05_170954_products_migration.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class ProductsMigration extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,17 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('tokenable');
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('sku');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->float('price');
+            $table->integer('quantity');
+
+
+            //foreign key for Category
+            $table->biginteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('category');
+
             $table->timestamps();
         });
     }
@@ -31,6 +35,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('products');
     }
 }
