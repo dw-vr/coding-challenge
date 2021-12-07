15faad6d4f738e55b0feb2367bce4f7e09cdd543
Roger G Coscojuela <admin@rogergili.com>
2021-12-07 23:27:59 +0100

    Created routes to API
    Created the requested routes both from product and category.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
-
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('/v1')->group(function(){
+    //there are two possible aproaches to limiting what methods are exposed in the api
+    //one is except, and the other is only
+    //['except'=>['edit','create'] ]
+    //['only'=>['edit','create'] ]
+    //for security reasons, we use only
+    Route::resource('categories',CategoryController::class,['only'=>['index','show'] ]);
+    Route::resource('products',ProductController::class,['only'=>['index','show','create','update','destroy'] ]);
 });
